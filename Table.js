4690ac59ded277d67eeddffa40c1ec34e53801dc
Yuer <1689416292@qq.com>
2018-11-17 15:42:14 +0800

    Add files via upload

---
+++ Table.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+const TableHeader = () => { 
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    );
+}
+const TableBody = props => { 
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+            </tr>
+        );
+    });
+
+    return <tbody>{rows}</tbody>;
+}
+class Table extends Component {
+    render() {
+        const { characterData } = this.props;
+        return (
+            <table>
+                <TableHeader />
+                <TableBody characterData={characterData} />
+            </table>
+        );
+    }
+}
+// 类组件必须包含render()，并且return只能返回一个父元素。
+// 注意，如果return包含在一行中，则不需要括号。
+export default Table;
